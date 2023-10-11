dc1a1c24bdbaf0ac32ae2662d560d5348a47e401
hussain07810991 <145981238+hussain07810991@users.noreply.github.com>
2023-10-11 23:17:53 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from flask import *
+app = Flask(__name__)
+@app.route("/AFRIT/Facebook")
+def say_hello():
+    return {"AFRIT":"Tle:@AFR_0 | @LPB_B","FACEBook":"23934021671137812393402167113781","FACEBOok":"699a699f696feaf2699a699f696feaf2","FACEBOOK":"66b8f18ca748626866b8f18ca7486268","FACEBOOk":"73400e380482b97f73400e380482b97f",}
+if __name__ == "__main__":
+    app.run()
